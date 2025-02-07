ee8354fefe2b05942428e29623cad756a1d9d853
Honor <bokseek@icloud.com>
2025-02-07 21:38:04 +0300

    вроде выполнил

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
+
         var dog=8.0;
         System.out.println(dog);
         var cat=3.6;
